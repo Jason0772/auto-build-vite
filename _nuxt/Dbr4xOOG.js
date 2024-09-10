0dbd5b2aebf30c9d5c047f549aa80748f3285dcc
Jason0772 <Jason0772@users.noreply.github.com>
2024-09-10 01:55:40 +0000

    deploy: 730bb35050d51ff3f453e981a077d6986a6416c4

---
+++ _nuxt/Dbr4xOOG.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./DlAUqK2U.js";import{r as s,x as a,y as o,z as c,v as r}from"./F2qTBu9o.js";const _={class:"container mx-auto p-4"},n={class:"mt-4 text-red-500"},p={__name:"test",setup(d){const e=s("");return e.value=void 0,(i,m)=>(r(),a("div",_,[o("p",n,"["+c(e.value)+"]",1)]))}},u=t(p,[["__scopeId","data-v-95989dfe"]]);export{u as default};
